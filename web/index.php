5e7e50165a0f487188b8e3326047c4f47f723253
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-16 12:53:32 -0700

    Added to PHP skeleton.

--- web/index.php
+++ web/index.php
@@ -1,31 +1,34 @@
 <?php
 
-/**
- * Step 1: Require the Slim Framework using Composer's autoloader
- *
- * If you are not using Composer, you need to load Slim Framework with your own
- * PSR-4 autoloader.
- */
 require '../vendor/autoload.php';
+require 'includes/init.php';
 
-/**
- * Step 2: Instantiate a Slim application
- *
- * This example instantiates a Slim application using
- * its default settings. However, you will usually configure
- * your Slim application now by passing an associative array
- * of setting names and values into the application constructor.
- */
 $app = new Slim\App();
 
-/**
- * Step 3: Define the Slim application routes
- *
- * Here we define several Slim application routes that respond
- * to appropriate HTTP request methods. In this example, the second
- * argument for `Slim::get`, `Slim::post`, `Slim::put`, `Slim::patch`, and `Slim::delete`
- * is an anonymous function.
- */
+//// USER ROUTES
+//$app->group('/users', function() use ($app) {
+//    $app->get('/', 'getUsers');
+//    $app->get('/bySite/:siteId', 'getUsersBySite');
+//    $app->get('/byPrimarySite/:siteId', 'getUsersByPrimarySite');
+//    $app->get('/bySecondarySite/:siteId', 'getUsersBySecondarySite');
+//    $app->get('/byPrimaryAndSecondarySite/:siteId', 'getUsersByPrimaryAndSecondarySite');
+//    $app->get('/admins', 'getAdmins');
+//    $app->get('/supervisors', 'getSupervisors');
+//    $app->get('/byId/:userId', 'getUserById');
+//    $app->post('/create', function() use ($app) {
+//        $u = json_decode($app->request()->getBody());
+//        createUser($u);
+//    });
+//    $app->post('/update', function() use ($app) {
+//        $u = json_decode($app->request()->getBody());
+//        updateUser($u);
+//    });
+//    $app->post('/delete', function() use ($app) {
+//        $u = json_decode($app->request()->getBody());
+//        deleteUser($u);
+//    });
+//});
+
 $app->get('/', function ($request, $response, $args) {
     $response->write("Welcome to Slim!");
     return $response;
@@ -36,12 +39,6 @@ $app->get('/hello[/{name}]', function ($request, $response, $args) {
     return $response;
 })->setArgument('name', 'World!');
 
-/**
- * Step 4: Run the Slim application
- *
- * This method should be called last. This executes the Slim application
- * and returns the HTTP response to the HTTP client.
- */
 $app->run();
 
 ?>
