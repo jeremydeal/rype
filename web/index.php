c5a9a56681edbcc5980f75ddcb04a5a898080aa9
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-17 13:29:22 -0700

    Changed Bootstrap links to CDN

--- web/index.php
+++ web/index.php
@@ -6,9 +6,15 @@ echo '
     <meta charset="utf-8">
     <title>My AngularJS App</title>
     <meta name="description" content="" />
-    <link rel="stylesheet" href="../vendor/twitter/bootstrap/dist/css/bootstrap.min.css" />
+
+    <!-- BOOTSTRAP LINKS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
     <link rel="stylesheet" href="app.css" />
 </head>
+
 <body class="container-fluid">
 
     <div class="navbar">
@@ -25,7 +31,6 @@ echo '
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-route.js"></script>
     <!--<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-ui/ui-bootstrap-tpls-0.14.3.min.js"></script>-->
-    <script src="../vendor/twitter/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="app.js"></script>
 
     <!-- CONTROLLERS -->
