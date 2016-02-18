e5725dfaeeb306b7f34a1eff331c558d7bdc223f
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-18 15:31:18 -0700

    fixed Angular architecture

--- web/index.php
+++ web/index.php
@@ -19,9 +19,15 @@ echo '
 
     <div class="navbar">
         <ul class="nav nav-pills">
+            <li><a class="active" href="#/test1">Home</a></li>
             <li><a href="#/produce">Produce</a></li>
-            <li><a href="#/test1">TEST 1</a></li>
-            <li><a href="#/test2">TEST 2</a></li>
+            <li><a href="#/test1">Grocery Store</a></li>
+            <form class="navbar-form navbar-right" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search Produce">
+                </div>
+                <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+            </form>
         </ul>
     </div>
 
