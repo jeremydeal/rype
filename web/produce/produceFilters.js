198d32001f36be2e70af87b763e7ea6c11d4a3c6
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-20 19:24:56 -0700

    refactor filters

--- web/produce/produceFilters.js
+++ web/produce/produceFilters.js
@@ -1,25 +1,24 @@
 app.filter('filterProduceByWhetherInSeason', function () {
     return function (products, inSeason) {
         // if inSeason is false, don't filter
-        if (inSeason == null || !inSeason) {
-            return products;
-        }
+        if (!products) return products;
+        if (!inSeason) return products;
 
         // dictionary of 0-indexed months to check current data against
         var months = ['january', 'february', 'march',
             'april', 'may', 'june', 'july', 'august',
             'september', 'october', 'november', 'december'];
 
+        var curMonth = new Date().getMonth();
+        var filtered = [];
+
         // iterate through products
-        var filtered = {};
-        for (var i = 0; i < products.length; i++) {
-            var p = products[i];
+        angular.forEach(products, function(p) {
 
             // get 0-indexed value of months
             var start = months.indexOf(p.SeasonStart.toLowerCase());
             var end = months.indexOf(p.SeasonEnd.toLowerCase());
 
-            var curMonth = new Date().getMonth();
 
             if (start && end) {
                 // add to filtered if the season is year-round
@@ -46,25 +45,25 @@ app.filter('filterProduceByWhetherInSeason', function () {
             else {
                 console.log("Item rejected: start or end dates not defined.");
             }
+        });
 
-        }
         return filtered;
     };
 });
 
 app.filter('filterProduceByType', function () {
     return function (products, typeId) {
-        if (typeId == null || typeId == "") {
-            return products;
-        }
+        if (!products) return products;
+        if (!typeId) return products;
 
-        var filtered = {};
-        for (var i = 0; i < products.length; i++) {
-            var p = products[i];
+        var filtered = [];
+
+        angular.forEach(products, function(p) {
             if (p.ProduceTypeId == typeId) {
                 filtered.push(p);
             }
-        }
+        });
+ 
         return filtered;
     };
 });
