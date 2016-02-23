245fbf0b48b232fc3a11619a3649bb3207651cbb
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-22 18:54:38 -0700

    Add Angular-Route for unit testing

---
+++ web/spec/productsControllerSpec.js
@@ -0,0 +1,20 @@
+describe('productsCotnroller', function(){
+
+    var $controllerConstructor;
+    var scope;
+
+    beforeEach(module('myApp'));
+
+    beforeEach(inject(function($controller, $rootScope){
+        $controllerConstructor = $controller;
+        scope = $rootScope.$new();
+    }));
+
+
+    it('should have three expense items', function(){
+        var ctrl = $controllerConstructor('expensesController', {$scope:scope});
+        expect(ctrl.expenseItems.length).toBe(3);
+    })
+
+
+})
