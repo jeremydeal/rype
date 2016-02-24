33aef326a7d16e648a777e357d0fdaef4bb9208e
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-24 01:57:27 -0700

    test createUser() API call

--- web/api/index.php
+++ web/api/index.php
@@ -3,7 +3,7 @@
 require '../../vendor/autoload.php';
 require 'includes/init.php';
 
-$app = new Slim\App(array("debug" => true));
+$app = new Slim\App(array("settings" => array("displayErrorDetails" => true)));
 
 // LOGIN ROUTES
 $app->group('/user/', function() use ($app) {
