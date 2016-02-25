9151aa6244d2f006a85b2eb99ef0b4fa1d390413
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-24 21:30:08 -0700

    testing!

--- web/api/includes/test.php
+++ web/api/includes/test.php
@@ -7,5 +7,6 @@ use Psr\Http\Message\ResponseInterface;
 function test1(ServerRequestInterface $request, ResponseInterface $response) {
     $data = $request->getParsedBody();
     $response->getBody()->write(json_encode($data));
+    $response = $response->withHeader('Content-Type', 'application/json; charset=utf-8');
     return $response;
 }
