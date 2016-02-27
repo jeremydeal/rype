2d50ab1c3f462a29f1a06ded0abbbaef5f227271
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-27 01:33:32 -0700

    test

--- web/session/authenticate_user.php
+++ web/session/authenticate_user.php
@@ -6,8 +6,26 @@ $user = json_decode(file_get_contents('php://input'));
 $email = $user->Email;
 $password = $user->Password;
 
-print $email . " and " . $password;
-//// authenticate user in database
+//print $email . " and " . $password;
+
+// authenticate user in database
+$sql = "SELECT u.*
+              FROM user AS u
+                WHERE u.Email = :email";
+try {
+    $db = getDB();
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam("email", $email);
+    $stmt->execute();
+    $dbUser= $stmt->fetch(PDO::FETCH_OBJ);
+    $db = null;
+    print '{"user": ' . json_encode($dbUser) . '}';
+} catch (PDOException $e) {
+    echo '{"error: { "text": ' . $e->getMessage() . '} }';
+}
+
+
+
 //$sql = "SELECT UserId, Email, Password
 //          FROM user
 //          WHERE Email = :email";
