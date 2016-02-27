8b12d0e3646f84c0552d7c8801c3c8dbf3ea20ec
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-26 22:12:02 -0700

    test )-:

--- web/app/services/loginService.js
+++ web/app/services/loginService.js
@@ -7,7 +7,7 @@ app.factory('loginService', function($http, $location, sessionService){
 		// verify user credentials
 		var $promise = $http.post('../session/authenticate_user.php', data);
 		$promise.then(function(msg) {
-			if(data) {
+			if(msg.data) {
 				var uid = msg.data.uid;
 
 				// store session unique id
