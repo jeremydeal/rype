861add6d47f7fb3e3d51ae635e0a83baa186dffc
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-27 00:50:56 -0700

    test

--- web/session/authenticate_user.php
+++ web/session/authenticate_user.php
@@ -5,8 +5,8 @@ require_once '../api/includes/db.php';
 $user = json_decode(file_get_contents('php://input'));
 
 // authenticate user in database
-$sql = "SELECT UserId, Email, Password
-          FROM heroku_adb66377d108485.[user]
+$sql = "SELECT *
+          FROM customer
           WHERE Email = :email";
 try {
     $db = getDB();
@@ -20,7 +20,7 @@ try {
     if (password_verify($user->Password, $dbUser->Password)) {
         // successful login; generate session
         session_start();
-        $_SESSION['uid'] = $dbUser->UserId;
+        $_SESSION['uid'] = $dbUser->CustomerId;
         print $_SESSION['uid'];
     }
 }
