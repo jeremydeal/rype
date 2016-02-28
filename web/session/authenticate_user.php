f191c336f274735722c9dca7366a0b5cb1f15237
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-27 17:10:24 -0700

    test

--- web/session/authenticate_user.php
+++ web/session/authenticate_user.php
@@ -3,31 +3,31 @@
 require_once 'db.php';
 
 $user = json_decode(file_get_contents('php://input'));
-$username = $user->Username;
-$password = $user->Password;
-
-//print $email . " and " . $password;
-
-// authenticate user in database
-$sql = "SELECT *
-              FROM heroku_adb66377d108485.user
-                WHERE Username = :username";
-try {
-    $db = getDB();
-    $stmt = $db->prepare($sql);
-    $stmt->bindParam("username", $username);
-    $stmt->execute();
-    $dbUser = $stmt->fetch(PDO::FETCH_OBJ);
-    $db = null;
-
-    print $dbUser->UserId;
-
-//    session_start();
-//    $_SESSION['uid'] = $dbUser->UserId;
-//    print $_SESSION['uid'];
-
-} catch (PDOException $e) {
-}
+//$username = $user->Username;
+//$password = $user->Password;
+//
+////print $email . " and " . $password;
+//
+//// authenticate user in database
+//$sql = "SELECT *
+//          FROM user
+//            WHERE Username = :username";
+//try {
+//    $db = getDB();
+//    $stmt = $db->prepare($sql);
+//    $stmt->bindParam("username", $username);
+//    $stmt->execute();
+//    $dbUser = $stmt->fetch(PDO::FETCH_OBJ);
+//    $db = null;
+//
+//    print $dbUser->UserId;
+//
+////    session_start();
+////    $_SESSION['uid'] = $dbUser->UserId;
+////    print $_SESSION['uid'];
+//
+//} catch (PDOException $e) {
+//}
 
 
 
@@ -44,9 +44,9 @@ try {
 ////}
 //
 //
-////
-////    if ($user->Email == "jeremy.n.deal@gmail.com" && $user->Password == "shadows1") {
-////        session_start();
-////        $_SESSION['uid'] = uniqid('ang_');
-////        print $_SESSION['uid'];
-////    }
+
+    if ($user->Email == "jeremy.n.deal@gmail.com" && $user->Password == "shadows1") {
+        session_start();
+        $_SESSION['uid'] = uniqid('ang_');
+        print $_SESSION['uid'];
+    }
