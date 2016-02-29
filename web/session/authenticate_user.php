77b33ac493a3f86ec3624d5147adcb64b4b48370
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-29 02:04:38 -0700

    test

--- web/session/authenticate_user.php
+++ web/session/authenticate_user.php
@@ -24,19 +24,12 @@ if ($user->Username && $user->Password) {
 //            print json_encode($dbUser);
 
             // we found a customer matching that username; authenticate the password
-            if (password_verify($user->Password, $dbUser->Password) == True)
+            if ($user->Password == $dbUser->Password)
             {
 //                session_start();
 //                $_SESSION['uid'] = $dbUser->CustomerId;
                 print json_encode($dbUser);
             }
-//            else {
-//                print "password lookup failed";
-//            }
-        }
-        else
-        {
-            print "no user was returned )-:";
         }
     }
     catch (PDOException $e) {
