e0e034dc8917dae6f81a9cfaaeea2935cf9297b6
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-28 23:15:33 -0700

    test

--- web/session/authenticate_user.php
+++ web/session/authenticate_user.php
@@ -4,30 +4,28 @@ require_once '../api/includes/db.php';
 
 $user = json_decode(file_get_contents('php://input'));
 
-print $user->Username . " " . $user->Password;
+if ($user->Username && $user->Password) {
 
+    // authenticate user in database
+    $sql = "SELECT * FROM customer WHERE Username = :username";
+    try {
+        $db = getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("username", $user->Username);
+        $stmt->execute();
+        $dbUser = $stmt->fetch(PDO::FETCH_OBJ);
+        $db = null;
 
-//if ($user->Username && $user->Password) {
-//
-//    // authenticate user in database
-//    $sql = "SELECT * FROM customer WHERE Username = :username";
-//    try {
-//        $db = getDB();
-//        $stmt = $db->prepare($sql);
-//        $stmt->bindParam("username", $user->Username);
-//        $stmt->execute();
-//        $dbUser = $stmt->fetch(PDO::FETCH_OBJ);
-//        $db = null;
-//
-//        // create and return the session
-//        session_start();
-//        $_SESSION['uid'] = uniqid('ang_');
+        // create and return the session
+        session_start();
+        $_SESSION['uid'] = uniqid('ang_');
 //        print $_SESSION['uid'];
-//    }
-//    catch (PDOException $e) {
-//        // DB access error; do not create a session
-//    }
-//}
+        print $user->Username . " " . $user->Password;
+    }
+    catch (PDOException $e) {
+        // DB access error; do not create a session
+    }
+}
 
 //// authenticate user in database
 //$sql = "SELECT *
