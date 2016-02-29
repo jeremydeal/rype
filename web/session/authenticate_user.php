0dccf3e6feefc9d2164ea8f80011edb4331d6bfb
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-29 01:54:11 -0700

    test

--- web/session/authenticate_user.php
+++ web/session/authenticate_user.php
@@ -21,15 +21,15 @@ if ($user->Username && $user->Password) {
         // create and return the session
         if (isset($dbUser))
         {
-            print json_encode($dbUser);
+//            print json_encode($dbUser);
 
-//            // we found a customer matching that username; authenticate the password
-//            if (isset($dbUser->Password) && password_verify($user->Password, $dbUser->Password))
-//            {
+            // we found a customer matching that username; authenticate the password
+            if (isset($dbUser->Password) && password_verify($user->Password, $dbUser->Password))
+            {
 //                session_start();
 //                $_SESSION['uid'] = $dbUser->CustomerId;
-//                print json_encode($dbUser);
-//            }
+                print json_encode($dbUser);
+            }
 //            else {
 //                print "password lookup failed";
 //            }
