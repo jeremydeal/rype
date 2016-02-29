1e450cb14a9e308443d88252010bf517691c5481
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-02-29 04:00:11 -0700

    login test on homepage

--- web/app/home/home.js
+++ web/app/home/home.js
@@ -1,8 +1,28 @@
 'use strict';
 
 app.controller('homeController',
-    ['$scope',
-        function($scope) {
+    ['$scope', 'sessionService',
+        function($scope, sessionService) {
+
+            // scope variables for login
+            $scope.user = {};
+
+            dataInit();
+
+
+            /////////////////////////////// DATA INITIALIZATION ////////////////////////////////////////////
+            function dataInit() {
+                populateUser();
+            }
+
+
+            /////////////////////////////// SERVICE CALLS ///////////////////////////////////////////////////
+            // populate user object from JS session
+            function populateUser() {
+                $scope.user.Username = sessionService.get("Username");
+                $scope.user.FirstName = sessionService.get("FirstName");
+                $scope.user.LastName = sessionService.get("LastName");
+            }
 
 
         }]);
