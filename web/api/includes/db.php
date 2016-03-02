51a8e5b4fd27238f3a8929ec0f807d1fe0643cd6
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-03-01 22:35:48 -0700

    reset DB connection vars to fix ClearDB timeouts?

--- web/api/includes/db.php
+++ web/api/includes/db.php
@@ -1,13 +1,22 @@
 <?php
 
 function getDB() {
-    $dbhost="us-cdbr-iron-east-03.cleardb.net";
-    $dbuser="b39303be9c0f98";
-    $dbpass="cfd24850";
-    $dbname="heroku_adb66377d108485";
+//    $dbhost="us-cdbr-iron-east-03.cleardb.net";
+//    $dbuser="b39303be9c0f98";
+//    $dbpass="cfd24850";
+//    $dbname="heroku_adb66377d108485";
+//
+//    $dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
 
-    $dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-    $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-    return $dbConnection;
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    $conn = new PDO($server, $username, $password, $db);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $conn;
 }
