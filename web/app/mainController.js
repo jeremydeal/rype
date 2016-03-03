3a289b537e584d118d1b0ce3e73f55ec10611be1
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-03-02 23:31:09 -0700

    refactor dead code out

--- web/app/mainController.js
+++ web/app/mainController.js
@@ -2,7 +2,7 @@
 
 app.controller('mainController',
     ['$scope', 'loginService',
-        function($scope, $location, loginService) {
+        function($scope, loginService) {
 
             // store current user's id, if available, to determine navbar appearance
             $scope.currentUserId = loginService.currentUserId;
