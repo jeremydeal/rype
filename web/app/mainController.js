6a866f7025be9a5403c9a124666be3cef357101f
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-03-02 23:55:00 -0700

    navbar fix

--- web/app/mainController.js
+++ web/app/mainController.js
@@ -1,10 +1,7 @@
 'use strict';
 
 app.controller('mainController',
-    ['$scope', 'loginService', '$rootScope',
-        function($scope, loginService, $rootScope) {
-
-            // store current user's id, if available, to determine navbar appearance
-            //$scope.currentUserId = $rootScope.currentUserId;
+    ['$scope', '$rootScope',
+        function($scope, $rootScope) {
 
         }]);
