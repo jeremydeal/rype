56bfb02eda3d9cd59f3a235b5dd482413af612d0
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-03-11 19:43:07 -0700

    add some functions to store.js -- I wonder what they do, and whether they'll work!!!

--- web/app/services/storesService.js
+++ web/app/services/storesService.js
@@ -5,20 +5,20 @@
     // handles all API calls to the Produce table on the backend
     app.factory('storesService', function ($http) {
         var baseUrl = "../api/store/";
-        var storesService = {};
+        var $storesService = {};
 
 
         // get all products
-        storesService.getStores = function () {
+        $storesService.getStores = function () {
             return $http.get(baseUrl);
         };
 
         // get one product
-        storesService.getStoreById = function (id) {
+        $storesService.getStoreById = function (id) {
             return $http.get(baseUrl + "byId/" + id);
         };
 
-        return storesService;
+        return $storesService;
     });
 
 })();
--- web/app/stores/stores.js
+++ web/app/stores/stores.js
@@ -3,8 +3,8 @@
     'use strict';
 
     app.controller('storesController',
-        ['$scope', 'storesService',
-            function ($scope, storesService) {
+        ['$scope', 'storesService', 'loginService', 'sessionService',
+            function ($scope, storesService, loginService, sessionService) {
 
                 // CONTROLLER GLOBALS
                 $scope.stores = {};
@@ -26,6 +26,7 @@
 
 
                 ///////////////////////////////////////////// SERVICE CALLS ///////////////////////////////////////////////
+                // STORES
                 function getStores() {
                     storesService.getStores()
                         .success(function (data) {
@@ -40,6 +41,25 @@
                         })
                 }
 
+                // CURRENT USER
+                $scope.isUserLoggedIn = function() {
+                    // get promise to check server-side user auth
+                    var connected = loginService.checkLoginStatus();
+
+                    // ...check to see if the user has a session registered
+                    connected.then(function(response){
+                        // if not logged in, redirect to login
+                        return !!response.data;
+                    });
+                };
+
+                $scope.isSelectedStore = function(storeId) {
+                    return sessionService.get("UserStore") != storeId;
+                };
+
+                $scope.setUserStore = function(storeId) {
+                    sessionService.set("UserStore", storeId);
+                };
 
 
             }]);
