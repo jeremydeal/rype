02581ba31f722480126b85737e7dd709d7e111e3
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-03-14 14:00:54 -0600

    set up google map

--- web/app/stores/stores.js
+++ web/app/stores/stores.js
@@ -26,38 +26,38 @@
 
 
                 ///////////////////////////////////// GOOGLE MAPS API ///////////////////////////////////////////////
-                var cities = [
-                    {
-                        city : 'Toronto',
-                        desc : 'This is the best city in the world!',
-                        lat : 43.7000,
-                        long : -79.4000
-                    },
-                    {
-                        city : 'New York',
-                        desc : 'This city is aiiiiite!',
-                        lat : 40.6700,
-                        long : -73.9400
-                    },
-                    {
-                        city : 'Chicago',
-                        desc : 'This is the second best city in the world!',
-                        lat : 41.8819,
-                        long : -87.6278
-                    },
-                    {
-                        city : 'Los Angeles',
-                        desc : 'This city is live!',
-                        lat : 34.0500,
-                        long : -118.2500
-                    },
-                    {
-                        city : 'Las Vegas',
-                        desc : 'Sin City...\'nuff said!',
-                        lat : 36.0800,
-                        long : -115.1522
-                    }
-                ];
+                //var cities = [
+                //    {
+                //        city : 'Toronto',
+                //        desc : 'This is the best city in the world!',
+                //        lat : 43.7000,
+                //        long : -79.4000
+                //    },
+                //    {
+                //        city : 'New York',
+                //        desc : 'This city is aiiiiite!',
+                //        lat : 40.6700,
+                //        long : -73.9400
+                //    },
+                //    {
+                //        city : 'Chicago',
+                //        desc : 'This is the second best city in the world!',
+                //        lat : 41.8819,
+                //        long : -87.6278
+                //    },
+                //    {
+                //        city : 'Los Angeles',
+                //        desc : 'This city is live!',
+                //        lat : 34.0500,
+                //        long : -118.2500
+                //    },
+                //    {
+                //        city : 'Las Vegas',
+                //        desc : 'Sin City...\'nuff said!',
+                //        lat : 36.0800,
+                //        long : -115.1522
+                //    }
+                //];
 
                 var mapOptions = {
                     zoom: 12,
@@ -70,28 +70,42 @@
 
                 var infoWindow = new google.maps.InfoWindow();
 
-                var createMarker = function (info){
+                var createMarkers = function(storeList) {
+                    for (var i = 0; i < storeList.length; i++){
+                        createMarker(storeList[i]);
+                    }
+                };
 
+                var createMarker = function (store){
+
+                    // create marker
                     var marker = new google.maps.Marker({
                         map: $scope.map,
-                        position: new google.maps.LatLng(info.lat, info.long),
-                        title: info.city
+                        position: new google.maps.LatLng(store.Latitude, store.Longitude),
+                        title: store.StoreName
                     });
-                    marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+                    // add StoreId as metedata so we can respond to user click events
+                    marker.metadata = { StoreId: store.StoreId };
+                    // fill info box that appears when user clicks a marker
+                    marker.content = '<h2>' + store.StoreName + '</h2>' +
+                        '<div class="infoWindowContent">' +
+                        store.Address + " " + store.City + ", " +
+                        store.State + " " + store.Zip
+                        + '</div>';
 
-                    google.maps.event.addListener(marker, 'click', function(){
-                        infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+                    // make info box appear on user click
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infoWindow.setContent(marker.content);
                         infoWindow.open($scope.map, marker);
                     });
 
                     $scope.markers.push(marker);
-
                 };
 
-                for (var i = 0; i < cities.length; i++){
-                    createMarker(cities[i]);
-                }
+                createMarkers($scope.stores);
 
+                // add click event to browser;
+                // note: click event added to map in createMarker()
                 $scope.openInfoWindow = function(e, selectedMarker){
                     e.preventDefault();
                     google.maps.event.trigger(selectedMarker, 'click');
