49e72d1f50f8d3818aab8a429ceea7654bdedba9
Kameron Perkins <Kameron Perkins>
2016-03-21 16:31:23 -0600

    Reworked prettyPring roundToTenth hopefully this is the last time.

--- web/app/services/prettyPrintService.js
+++ web/app/services/prettyPrintService.js
@@ -69,7 +69,7 @@
 
 
         prettyPrintService.roundToTenth = function (num) {
-            return Math.round(i * 10)/10;
+            return Math.round(num * 10)/10;
         };
 
         return prettyPrintService;
