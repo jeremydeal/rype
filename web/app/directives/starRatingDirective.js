afc13e6292a48b05ffa7bdcb0de960633252b393
Jeremy Deal <jeremy.n.deal@gmail.com>
2016-04-09 17:18:50 -0600

    build star rating directive JS

--- web/app/directives/starRatingDirective.js
+++ web/app/directives/starRatingDirective.js
@@ -18,53 +18,53 @@
 
         directive.templateUrl = "app/directives/starRatingDirective.html";
 
-        // directive.link = function(scope, elements, attr) {
-        //
-        //     scope.updateStars = function() {
-        //         var idx = 0;
-        //         scope.stars = [ ];
-        //         for (idx = 0; idx < scope.max; idx += 1) {
-        //             scope.stars.push({
-        //                 full: scope.score > idx
-        //             });
-        //         }
-        //     };
-        //
-        //     scope.hover = function(/** Integer */ idx) {
-        //         scope.hoverIdx = idx;
-        //     };
-        //
-        //     scope.stopHover = function() {
-        //         scope.hoverIdx = -1;
-        //     };
-        //
-        //     scope.starColor = function(/** Integer */ idx) {
-        //         var starClass = 'rating-normal';
-        //         if (idx <= scope.hoverIdx) {
-        //             starClass = 'rating-highlight';
-        //         }
-        //         return starClass;
-        //     };
-        //
-        //     scope.starClass = function(/** Star */ star, /** Integer */ idx) {
-        //         var starClass = 'fa-star-o';
-        //         if (star.full || idx <= scope.hoverIdx) {
-        //             starClass = 'fa-star';
-        //         }
-        //         return starClass;
-        //     };
-        //
-        //     scope.setRating = function(idx) {
-        //         scope.score = idx + 1;
-        //         scope.stopHover();
-        //     };
-        //
-        //     scope.$watch('score', function(newValue, oldValue) {
-        //         if (newValue !== null && newValue !== undefined) {
-        //             scope.updateStars();
-        //         }
-        //     });
-        // };
+        directive.link = function(scope, elements, attr) {
+
+            scope.updateStars = function() {
+                scope.stars = [];
+                for (var starId = 0; starId < scope.max; starId += 1) {
+                    scope.stars.push({
+                        full: scope.score > starId
+                    });
+                }
+            };
+
+            scope.hover = function(/* Integer */ index) {
+                scope.hoverIndex = index;
+            };
+
+            scope.stopHover = function() {
+                scope.hoverIndex = -1;
+            };
+
+            scope.starColor = function(/* Integer */ index) {
+                var starClass = 'rating-normal';
+                if (index <= scope.hoverIndex) {
+                    starClass = 'rating-highlight';
+                }
+                return starClass;
+            };
+
+            scope.starClass = function(/* Star */ star, /* Integer */ index) {
+                var starClass = 'fa-star-o';
+                if (star.full || index <= scope.hoverIndex) {
+                    starClass = 'fa-star';
+                }
+                return starClass;
+            };
+
+            scope.setRating = function(index) {
+                scope.score = index + 1;
+                scope.stopHover();
+            };
+
+            scope.$watch('score', function(newValue, oldValue) {
+                if (newValue !== null && newValue !== undefined) {
+                    scope.updateStars();
+                }
+            });
+
+        };
 
         return directive;
 
